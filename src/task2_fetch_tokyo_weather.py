889518370091c3261f17a12e43b5799043b90e94
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2025-01-05 16:26:47 +0200

    Update task2_fetch_tokyo_weather.py

--- src/task2_fetch_tokyo_weather.py
+++ src/task2_fetch_tokyo_weather.py
@@ -9,19 +9,8 @@ def fetch_weather_data():
     Returns:
         dict: A dictionary containing the date and the maximum temperature.
     """
-    url = "https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&daily=temperature_2m_max" \
-          "&timezone=Asia/Tokyo"
-    response = requests.get(url)
-    response.raise_for_status()  # Check that the request was successful
-
-    # Parse the JSON response
-    data = response.json()
-
-    # Extract the date and maximum temperature
-    date = data['daily']['time'][0]
-    max_temperature = data['daily']['temperature_2m_max'][0]
-
-    return {"date": date, "max_temperature": max_temperature}
+   
+    return None
 
 
 def save_to_json(data, filename):
@@ -32,8 +21,7 @@ def save_to_json(data, filename):
         data (dict): The data to be saved.
         filename (str): The name of the JSON file.
     """
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
+    ...
 
 
 if __name__ == "__main__":
